ca5431950619446d57a3c77a228a4c76940ad71e
amadeusk1 <141212854+amadeusk1@users.noreply.github.com>
2026-07-11 15:22:20 -0600

    keep fuel log entry in store

--- app/src/main/java/com/fuelcheck/FuelLogEntry.kt
+++
@@ -1,9 +0,0 @@
-package com.fuelcheck
-
-data class FuelLogEntry(
-    val timestampMs: Long,
-    val odometerKm: Double,
-    val litersAdded: Double,
-    val remainingLiters: Double,
-    val isFullTank: Boolean
-)
